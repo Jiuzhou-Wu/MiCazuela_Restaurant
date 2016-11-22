dc560fd3d1532fde39deb06eab4c8faa8c3d1d83
Jiuzhou <nova2357@gmail.com>
2016-11-22 16:47:01 -0500

    PartyOrderList Done(need to add initialization in main function)

--- src/mrSimModel/PartyOrderList.java
+++ src/mrSimModel/PartyOrderList.java
@@ -1,10 +1,39 @@
 package mrSimModel;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * 
  * @author Jiuzhou Wu
  *
  */
 public class PartyOrderList {
-
+	LinkedBlockingQueue<Party> partyOrderList;
+	int n;
+	
+	public PartyOrderList(int n){
+		// n means the number of tables
+		this.partyOrderList = new LinkedBlockingQueue<Party>(n);
+		n = 0;
+	}
+	
+	public Party pickOrder(){
+		n--;
+		return partyOrderList.remove();
+	}
+	
+	public boolean addOrder(Party order){
+		boolean flag = false;
+		if(partyOrderList.remainingCapacity() != 0){
+			partyOrderList.add(order);
+			n++;
+			flag = true;
+		}
+		return flag;
+	}
+	
+	public int getNumOfOrders(){
+		return n;
+	}
+	
 }
