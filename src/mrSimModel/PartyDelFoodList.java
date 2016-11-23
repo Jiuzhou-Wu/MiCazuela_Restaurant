1466624dd7d1c49bc8b4af5cd18cd9874e3550d1
Jiuzhou <nova2357@gmail.com>
2016-11-22 20:14:52 -0500

    Finished PartyDelFoodList (Need to add in main and initialization)

--- src/mrSimModel/PartyDelFoodList.java
+++ src/mrSimModel/PartyDelFoodList.java
@@ -1,10 +1,38 @@
 package mrSimModel;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * 
  * @author Jiuzhou Wu
  *
  */
 public class PartyDelFoodList {
-
+	LinkedBlockingQueue<Party> FoodDyList;
+	int n;
+	
+	public PartyDelFoodList(int n){
+		// n means the number of tables
+		this.FoodDyList = new LinkedBlockingQueue<Party>(n);
+		n = 0;
+	}
+	
+	public Party FoodReadyDy(){
+		n--;
+		return FoodDyList.remove();
+	}
+	
+	public boolean foodNeedCook(Party order){
+		boolean flag = false;
+		if(FoodDyList.remainingCapacity() != 0){
+			FoodDyList.add(order);
+			n++;
+			flag = true;
+		}
+		return flag;
+	}
+	
+	public int getNumOfOrders(){
+		return n;
+	}
 }
