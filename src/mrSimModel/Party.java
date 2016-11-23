879605747cfed414bfca286354313f63ff70644a
Jiuzhou <nova2357@gmail.com>
2016-11-22 19:31:40 -0500

    Merge

--- src/mrSimModel/Party.java
+++ src/mrSimModel/Party.java
@@ -1,20 +1,28 @@
 package mrSimModel;
 
+import mrSimModel.Party.PartyScale;
+
 /*
  *  This consumer entity structure represents the party 
  *  arrive at the restaurant, being served, paying and then 
  *  leaving the restaurant
  */
 public class Party {
-	
+	//attributes
+		int uSize;
+		PartyScale thisPartyScale;
+		double startWait;
+		double waitTime;
 	enum PartyScale{
 		LARGE, SMALL
 	}
+	public Party(int uSize, double startWait){
+		waitTime = 0;
+		if (uSize < 3)
+			thisPartyScale = PartyScale.SMALL;
+		else
+			thisPartyScale = PartyScale.LARGE;
+	}
 	
-	//attributes
-	int uSize;
-	PartyScale thisPartyScale;
-	double startWait;
-	double waitTime;
 	
 }
